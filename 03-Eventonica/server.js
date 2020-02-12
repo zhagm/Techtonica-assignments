9b7ff955afa551f742531287cb6f933083e066ac
zhagm <zhagdev@gmail.com>
2020-02-11 16:31:26 -0800

    Modify routes to switch case

--- 03-Eventonica/server.js
+++ 03-Eventonica/server.js
@@ -1,23 +1,23 @@
-var http = require("http");
-var fs = require("fs");
+let { createServer } = require("http");
+let fs = require("fs");
 
-http
-  .createServer(function(request, response) {
-    var filePath = "." + request.url;
-    if (filePath == "./") {
+createServer(function(req, res) {
+  let filePath = "." + req.url;
+
+  switch (filePath.toLowerCase()) {
+    case "./":
       filePath = "./index.html";
-    } else if (filePath == "./gabby") {
+      break;
+    case "./gabby":
       filePath = "./Gabby/gabby.html";
-    } else if (filePath == "./ariel") {
+      break;
+    case "./ariel":
       filePath = "./ariel.html";
-    } else {
+      break;
+    default:
       filePath = "./error.html";
-    }
+  }
 
-    fs.readFile(filePath, function(error, content) {
-      response.writeHead(200);
-      response.end(content, "utf-8");
-    });
-  })
-  .listen(8125);
+  fs.readFile(filePath, (e, content) => res.end(content));
+}).listen(8125);
 console.log("Server running at http://127.0.0.1:8125/");
