72d081cf25e03ee7801424d0d8a2c3611e26cb70
zhagm <zhagdev@gmail.com>
2020-02-13 10:12:40 -0800

    Fix server issue with error.html

--- 04-Eventonica-API/server.js
+++ 04-Eventonica-API/server.js
@@ -1,15 +1,30 @@
-let { createServer } = require("http");
+let http = require("http");
 let fs = require("fs");
 
-createServer(function(req, res) {
-  switch (req.url.toLowerCase()) {
-    case "/":
-      filePath = "./index.html";
-      break;
-    default:
-      filePath = "./error.html";
-  }
+const PORT = 3000;
 
-  fs.readFile(filePath, (e, content) => res.end(content));
-}).listen(8125);
-console.log("Server running at http://127.0.0.1:8125/");
+http
+  .createServer(function(request, response) {
+    console.log("request ", request.url);
+
+    const url = request.url;
+
+    console.log("url =>", url);
+
+    let filePath;
+    if (url == "/") {
+      filePath = "index.html";
+    } else {
+      filePath = "." + url;
+    }
+
+    console.log(`filePath => '${filePath}'`);
+
+    fs.readFile(filePath, function(error, content) {
+      console.log({ error, content });
+      response.end(content);
+    });
+  })
+  .listen(PORT);
+
+console.log(`Server running at http://127.0.0.1:${PORT}/`);
